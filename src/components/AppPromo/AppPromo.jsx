1a6cdf0baa79e3ee283cbe3a9ba2a9290c25c100
fuzenblue <primrosemprst@gmail.com>
2024-09-20 01:43:05 +0700

    seconde update
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   index.html
    	modified:   src/components/AppPromo/AppPromo.css
    	modified:   src/components/AppPromo/AppPromo.jsx

--- src/components/AppPromo/AppPromo.jsx
+++ src/components/AppPromo/AppPromo.jsx
@@ -1,21 +1,66 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 import './AppPromo.css'
 
 const AppPromo = () => {
-  return (
-    <div className="app-promo" id="app-promo">
-      <div class="wrap">
-        <h1>To-Do List</h1>
+  const [tasks, setTasks] = useState([]);
+  const [taskText, setTaskText] = useState('');
 
-        <form action="#" class="formInput">
-            <input type="text" class="task" id="taskEnter" placeholder="Enter Your Task" />
-            <button type="submit" class="addBtn" id="addTask">Add</button>
-            <button class="clearBtn" id="clearCompleted">Clear Completed</button>
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    setTasks(storedTasks);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = () => {
+    if (taskText !== '') {
+      const newTask = { text: taskText, completed: false };
+      setTasks([...tasks, newTask]);
+      setTaskText('');
+    }
+  };
+
+  const toggleTaskCompletion = (index) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const clearCompletedTasks = () => {
+    const activeTasks = tasks.filter(task => !task.completed);
+    setTasks(activeTasks);
+  };
+
+  return (
+    <div className='app-promo' id='app-promo'>
+      <div className="wrap">
+        <h2>Task List</h2>
+        <form action="#app-promo" className="formInput">
+          <input type="text" value={taskText} onChange={(e) => setTaskText(e.target.value)} placeholder="Enter task" />
+          <button className='addBtn' onClick={addTask}>Add Task</button>
+          <button className='clearBtn' onClick={clearCompletedTasks}>Clear Completed</button>
         </form>
+
+        <ul id="input-result">
+          {tasks.map((task, index) => (
+            <li className='task-List'
+              key={index}
+              onClick={() => toggleTaskCompletion(index)}
+              style={{ 
+                textDecoration: task.completed ? 'line-through' : 'none',
+                color: task.completed ? 'gray' : 'black',
+                cursor: 'pointer'
+              }}
+            >
+              {task.text}
+            </li>
+          ))}
+        </ul>
         
-        <div class="input-result">
-            <ul id="taskList" class="task-List"></ul>
-        </div>
       </div>
     </div>
   )
